be2a84706407069066e7778ade73240455ed942d
Koray <74928882+koraydincc@users.noreply.github.com>
2023-05-05 22:07:10 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+durum = '';
+
 document.getElementById('hesapla').addEventListener('click', function(){
 
 
     
     let kg = Math.abs(document.getElementById('inp-kg').value);
     let cm = Math.abs(document.getElementById('inp-cm').value);
+    let img = document.getElementById('image1') ;
+    
+
+    
 
     if (kg == '') {
         alert('Lütfen kilo bilginizi giriniz!')
@@ -16,25 +22,83 @@ document.getElementById('hesapla').addEventListener('click', function(){
     }
 
     sonuc =(kg*100)/((cm*cm)/100);
-
-    document.getElementById('sonuc').innerHTML = `${sonuc.toFixed(2)}`; //toFixed virgülden sonrasını alır
     
-    /*
-      0 - 18.4 Zayıf
-      18.5 -24.9 Normal
-      25 - 29.9 Fazla Kilolu
-      30 - 34.9 Şişman(Obez) - 1.sınıf
-      35 - 44.5 Şişman(obez) - 2.sınıf
-      45 ve üstü Aşırı şişman(obez ) 3.sınıf
-    */
 
+
+    //CM-KG-ZAYIF-NORMAL GİBİ ÖNEMLİ KELİMELERİ KALIN YAZMAK İSTEDİM AMA YAPAMADIM
+    if (sonuc<18.5) {
+
+        img.style.display = 'inline-block'
+
+        durum = 'Zayıf';
+
+            
+        let uyari = `Boyunuzu ${cm} cm, kilonuzu ${kg} kg olarak belirttiniz. Bu değerlere göre Vücut Kitle İndeksiniz (VKİ) ${sonuc.toFixed(2)} kg/m² olarak hesaplandı.VKİ değerinize göre tıbben ZAYIF kategorisindesiniz. Sağlıklı bir şekilde ideal kilonuza ulaşabilmeniz için lütfen bir kliniğe başvurunuz.`
+
+        document.getElementById('uyari').innerHTML = `${uyari}`;
+        
+        
+    }
+    if (sonuc >= 18.5 && sonuc <=25 ) {
+
+        durum = 'Normal'
+
+        let uyari = `Boyunuzu ${cm} cm, kilonuzu ${kg} kg olarak belirttiniz. Bu değerlere göre Vücut Kitle İndeksiniz (VKİ) ${sonuc.toFixed(2)} kg/m² olarak hesaplandı.VKİ değerinize göre tıbben  kategorisindesiniz. Mevcut koşullarda kilonuzla ilgili bir sağlık sorununuz olması beklenmiyor.Sizi tebrik ediyor, bundan sonraki yaşamınızda da formunuzu korumanızı diliyoruz.`
+
+        document.getElementById('uyari').innerHTML = `${uyari}`;
+        
+    } 
+    if (sonuc >= 25 && sonuc < 30) {
+
+        durum = 'Fazla Kilolu'
+
+
+        let uyari = `Boyunuzu ${cm} cm, kilonuzu ${kg} kg olarak belirttiniz. Bu değerlere göre Vücut Kitle İndeksiniz (VKİ) ${sonuc.toFixed(2)} kg/m² olarak hesaplandı.VKİ değerinize göre tıbben FAZLA KİLOLU kategorisindesiniz. Size medikal tedavi öneriyoruz. Diyet ve Egzersiz yaparak zayıflayabilirsiniz.Sağlıklı bir şekilde ideal kilonuza ulaşabilmeniz için lütfen bir kliniğe başvurunuz.`
+
+        document.getElementById('uyari').innerHTML = `${uyari}`;
+        
+    }
+    if (sonuc >=30 && sonuc<35) {
+
+        durum = 'I.Derece Obezite'
+
+
+        let uyari = `Boyunuzu ${cm} cm, kilonuzu ${kg} kg olarak belirttiniz. Bu değerlere göre Vücut Kitle İndeksiniz (VKİ) ${sonuc.toFixed(2)} kg/m² olarak hesaplandı.VKİ değerinize göre tıbben I. DERECE OBEZİTE kategorisindesiniz. Size medikal tedavi öneriyoruz. Diyet ve Egzersiz yaparak zayıflayabilirsiniz.Sağlıklı bir şekilde ideal kilonuza ulaşabilmeniz için lütfen bir kliniğe başvurunuz.`
+
+    }
+    if (sonuc >=35 && sonuc<45) {
+        durum = 'II.Derece Obezite'
+
+
+        let uyari = `Boyunuzu ${cm} cm, kilonuzu ${kg} kg olarak belirttiniz. Bu değerlere göre Vücut Kitle İndeksiniz (VKİ) ${sonuc.toFixed(2)} kg/m² olarak hesaplandı.VKİ değerinize göre tıbben II. DERECE OBEZİTE kategorisindesiniz. Size medikal tedavi öneriyoruz. Diyet ve Egzersiz yaparak zayıflayabilirsiniz.Sağlıklı bir şekilde ideal kilonuza ulaşabilmeniz için lütfen bir kliniğe başvurunuz.`
+
+    }
+    if (sonuc >=35 && sonuc<45) {
+
+        durum = 'III.Derece Obezite'
+
+        let uyari = `Boyunuzu ${cm} cm, kilonuzu ${kg} kg olarak belirttiniz. Bu değerlere göre Vücut Kitle İndeksiniz (VKİ) ${sonuc.toFixed(2)} kg/m² olarak hesaplandı.VKİ değerinize göre tıbben III. DERECE OBEZİTE kategorisindesiniz.Size obezite cerrahisi öneriyoruz. Ameliyat olarak sağlıklı bir şekilde zayıflayabilirsiniz.Tedavi için lütfen obezite cerrahisi uygulanan bir Genel Cerrahi Kliniğine başvurunuz.`
+
+    }
+ 
+        
+    document.getElementById('sonuc').innerHTML = `${sonuc.toFixed(2)} ${durum}`; //toFixed virgülden sonrasını alır
+    
 })
 
 document.getElementById('clear').addEventListener('click',function(){
 
+    let img = document.getElementById('image1') ;
+
     document.getElementById('inp-kg').value='';
     document.getElementById('inp-cm').value='';
     document.getElementById('sonuc').innerHTML='';
+    document.getElementById('uyari').innerHTML = '';
+    document.getElementById('image1').src = '';
+
+    img.style.display = 'none';
+    
+
 
   
 
